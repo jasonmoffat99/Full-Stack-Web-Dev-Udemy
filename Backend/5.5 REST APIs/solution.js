4a4c5ef5d52c5ad441e4af1715941e2aa176441a
Jason <jasonmoffat1999@gmail.com>
2023-12-07 11:39:57 +0000

    Completed REST API module 5.4 and 5.5

--- Backend/5.5 REST APIs/solution.js
+++ Backend/5.5 REST APIs/solution.js
@@ -7,7 +7,7 @@ const port = 3000;
 const API_URL = "https://secrets-api.appbrewery.com";
 
 //Add your own bearer token from the previous lesson.
-const yourBearerToken = "08f3026d-9c6c-4d88-a3b2-c579dc106247";
+const yourBearerToken = "49449b48-53a4-463d-bb9a-1ad970a2e0e3";
 const config = {
   headers: { Authorization: `Bearer ${yourBearerToken}` },
 };
